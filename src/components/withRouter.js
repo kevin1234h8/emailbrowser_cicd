a351195ce531167c47df6035669491baa7635d1b
unknown <kevinang@gmail.com>
2024-05-10 15:21:06 +0800

    install and build

--- src/components/withRouter.js
+++ src/components/withRouter.js
@@ -1,16 +1,11 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export const withRouter = (Component) => {
   const Wrapper = (props) => {
     const history = useNavigate();
-    
-    return (
-      <Component
-        history={history}
-        {...props}
-        />
-    );
+
+    return <Component history={history} {...props} />;
   };
-  
+
   return Wrapper;
 };
