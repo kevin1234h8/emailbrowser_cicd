8074905ddac330d9ad318b49cb3e1271cc7304e0
unknown <kevinang@gmail.com>
2024-05-14 16:43:09 +0800

    sonarqube scanner 3000

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,20 +1,20 @@
-import scanner from "sonarqube-scanner"
+import scanner from "sonarqube-scanner";
 scanner(
-    {
-        serverUrl: "http://localhost:9000",
-        options: {
-            "sonar.login": process.env.REACT_SONAR_USERNAME,
-            "sonar.password": process.env.REACT_SONAR_PASSWORD,
-            "sonar.projectName": "react_app",
-            "sonar.projectDescription": "Just for demo...",
-            "sonar.sourceEncoding":"UTF-8",
-            "sonar.sources": ".",
-            "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
-            "sonar.exclusions": "**/*.test.tsx",
-            "sonar.tests":"./src",
-            "sonar.testExecutionReportPaths":"test-report.xml",
-            "sonar.javascript.lcov.reportPaths":"coverage/lcov.info"
-        },
+  {
+    serverUrl: "http://localhost:3000",
+    options: {
+      "sonar.login": process.env.REACT_SONAR_USERNAME,
+      "sonar.password": process.env.REACT_SONAR_PASSWORD,
+      "sonar.projectName": "react_app",
+      "sonar.projectDescription": "Just for demo...",
+      "sonar.sourceEncoding": "UTF-8",
+      "sonar.sources": ".",
+      "sonar.test.inclusions": "**/*.test.tsx,**/*.test.ts",
+      "sonar.exclusions": "**/*.test.tsx",
+      "sonar.tests": "./src",
+      "sonar.testExecutionReportPaths": "test-report.xml",
+      "sonar.javascript.lcov.reportPaths": "coverage/lcov.info",
     },
-    () => process.exit()
+  },
+  () => process.exit()
 );
